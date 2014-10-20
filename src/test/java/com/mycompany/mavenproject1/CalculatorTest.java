c7b3f7b7d3749ad831c3b70a78200273221e2abc
Gaba <feltserg@gmail.com>
2014-10-20 11:07:00 +0200

    initial import

---
+++ src/test/java/com/mycompany/mavenproject1/CalculatorTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gaba
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @org.junit.Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 12;
+        int b = 30;
+        Calculator instance = new Calculator();
+        int expResult = 42;
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+    }
+    
+}
